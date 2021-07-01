3c99fff8dba002bb4769fe9e0b8d2480f18b2ced
Christian Hernandez <christiansv10@hotmail.com>
2021-06-30 21:36:47 -0600

    Se aplico estilo y se instalo leafletjs

--- src/components/Search.js
+++ src/components/Search.js
@@ -2,17 +2,59 @@ import styled from '@emotion/styled';
 
 const Container = styled.div`
     background-image: url('/images/pattern-bg.png');
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
 `;
 
 const Title = styled.h1`
     margin: 0;
+    padding-top: 30px;
     color: white;
 `;
+
+const Search = styled.div`
+    margin-top: 30px;
+    margin-bottom: 50px;
+`;
+
+const SearchBar = styled.input`
+    font-size: 18px;
+    padding: 10px;
+    border: none;
+    border-radius: 10px 0px 0px 10px;
+    width: 400px;
+    :focus{
+        outline: none;
+    }
+`;
+
+const SearchBtn = styled.input`
+    padding: 10px;
+    font-size: 18px;
+    border: none;
+    border-radius: 0px 10px 10px 0px;
+    color: #ffffff;
+    background-color: black;
+    cursor: pointer;
+`;
+
+const sendIp = () =>{
+    console.log('Holiss');
+}
 
 const header = () => {
     return (
         <Container>
             <Title>IP Address Tracker</Title>
+            <Search>
+                <SearchBar type="text" placeholder='Search for any IP address or domain'></SearchBar>
+                <SearchBtn
+                    type='submit'
+                    value='>'
+                    onClick={() => sendIp()}
+                ></SearchBtn>
+            </Search>
         </Container>
     );
 }
